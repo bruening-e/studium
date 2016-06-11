3157a07188708d239f5cb28f1f083f37f8df2115
bruening-e <Eric.Bruening@dife.de>
2016-06-11 17:35:10 +0200

    Build
    nach der präsens

---
+++ Build Script/src/de/bruening/Code.java
@@ -0,0 +1,9 @@
+package de.bruening;
+/**
+*	@author Eric Bruening
+*	@E-mail Eric.Bruening@dife.de
+**/
+
+public class Code {
+
+}
---
+++ Build Script/src/de/bruening/morecode.java
@@ -0,0 +1,8 @@
+package de.bruening;
+/**
+*	@author Eric Bruening
+*	@E-mail Eric.Bruening@dife.de
+**/
+public class morecode {
+
+}
