1bdae224a4f80daac9ef5db4e0b2b6dbae268d9a
bruening-e <Eric.Bruening@dife.de>
2016-06-11 19:09:12 +0200

    weiter geht es

--- Build Script/src/de/bruening/Code.java
+++ Build Script/src/de/bruening/Code.java
@@ -6,4 +6,9 @@ package de.bruening;
 
 public class Code {
 
+    public static void main (String[] args)
+    {
+         
+          System.out.println("Hello World!");
+    }
 }
--- Build Script/src/de/bruening/morecode.java
+++
@@ -1,8 +0,0 @@
-package de.bruening;
-/**
-*	@author Eric Bruening
-*	@E-mail Eric.Bruening@dife.de
-**/
-public class morecode {
-
-}
