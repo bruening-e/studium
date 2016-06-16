aab7005ef16f66aa2cd9f0aa60143f5edf8671af
bruening-e <Eric.Bruening@dife.de>
2016-06-16 22:01:48 +0200

    remove deprecation

--- LE DEP/src/de/eb/CalculatorTest.java
+++ LE DEP/src/de/eb/CalculatorTest.java
@@ -1,17 +1,17 @@
 package de.eb;
 
-import junit.framework.Assert;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-@SuppressWarnings("deprecation")
+
 public class CalculatorTest {
 
  @Test
  public void testAverage() {
   Injector i = Guice.createInjector();
   Calculator calci = i.getInstance(Calculator.class);
-  Assert.assertEquals(4, calci.average(2,6));
+  assertEquals(4, calci.average(2,6));
   }
 }
